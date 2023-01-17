6be2bd84ce53e1c00beeb34aac999bc84ad6f456
kaasikodes <thisbroisfresh@gmail.com>
2023-01-17 23:53:24 +0100

    setting up boiler plate for classTeacherRecords

--- src/components/classes/settings/SetupClassesTeacherForSession.tsx
+++ src/components/classes/settings/SetupClassesTeacherForSession.tsx
@@ -1,37 +1,54 @@
-import { Form, Button, Typography, Checkbox, Input, Select } from "antd";
-import React from "react";
+import { Form, Button, Typography, Checkbox, Input, Select, Spin } from "antd";
+import pagination from "antd/lib/pagination";
+import React, { useContext, useState } from "react";
+import { useAuthUser } from "react-auth-kit";
+import { IAuthDets } from "../../../appTypes/auth";
+import { GlobalContext } from "../../../contexts/GlobalContextProvider";
+import { useFetchClasses } from "../../../helpersAPIHooks/classes";
+import { useFetchAllStaff } from "../../../helpersAPIHooks/staff";
 import { IClassEntry } from "../ClassesTable";
 
 const SetupClassesTeacherForSession = () => {
+  const auth = useAuthUser();
+
+  const authDetails = auth() as unknown as IAuthDets;
+
+  const token = authDetails.userToken;
+
+  const globalCtx = useContext(GlobalContext);
+  const { state: globalState } = globalCtx;
+  const schoolId = globalState?.currentSchool?.id as string;
+  const [staffHint, setStaffHint] = useState<string | undefined>();
+
+  const { data: levelsData } = useFetchClasses({
+    schoolId,
+    token,
+    pagination: {
+      limit: 200,
+
+      page: 1,
+    },
+    // searchParams: {
+    //   name: searchTerm,
+    // },
+  });
+
+  const { data: staffData, isSuccess: isStSucess } = useFetchAllStaff({
+    schoolId,
+    token,
+    pagination: {
+      limit: 200,
+
+      page: 1,
+    },
+    searchParams: {
+      name: staffHint,
+    },
+  });
+
   const handleSubmit = (data: any) => {
-    console.log("DATA TEACH", data);
+    console.log("ASS STAFF", data);
   };
-  const levels: IClassEntry[] = [
-    {
-      name: "Jss1",
-      courseCount: 0,
-      id: "0",
-      studentCount: 0,
-      teacherCount: 0,
-      description: "",
-    },
-    {
-      name: "Jss2",
-      courseCount: 0,
-      id: " 1",
-      studentCount: 0,
-      teacherCount: 0,
-      description: "",
-    },
-    {
-      name: "Jss3",
-      courseCount: 0,
-      id: "3",
-      studentCount: 0,
-      teacherCount: 0,
-      description: "",
-    },
-  ];
   return (
     <div>
       {" "}
@@ -48,20 +65,36 @@ const SetupClassesTeacherForSession = () => {
             </Typography.Title>
 
             <div className="grid md:grid-cols-4 grid-cols-1 gap-4">
-              {levels.map((item: IClassEntry) => (
+              {levelsData?.data.map((item: IClassEntry) => (
                 <Form.Item
                   name={`${item.id}`}
                   label={item.name}
                   labelCol={{ span: 24 }}
                   // wrapperCol={{ span: 24 }}
                 >
-                  <Select key={item.id} placeholder={`${item.name}`}>
-                    <Select.Option value="1" key={"1"}>
-                      test
-                    </Select.Option>
-                    <Select.Option value="2" key={"2"}>
-                      test2
-                    </Select.Option>
+                  <Select
+                    onSearch={(val) => setStaffHint(val)}
+                    showSearch
+                    value={staffHint}
+                    defaultActiveFirstOption={false}
+                    showArrow={false}
+                    filterOption={false}
+                    // onChange={handleChange}
+                    notFoundContent={null}
+                  >
+                    {/* TO DO
+            Convert all select to api searchable dropdowns */}
+                    {isStSucess ? (
+                      staffData?.data.map((item) => (
+                        <Select.Option key={item.id} value={item.id}>
+                          {item.name}({item.staffNo})
+                        </Select.Option>
+                      ))
+                    ) : (
+                      <div className="flex justify-center items-center w-full">
+                        <Spin size="small" />
+                      </div>
+                    )}
                   </Select>
                 </Form.Item>
               ))}
