2f112a75b8a748872053c1ff46764ed48818418d
kaasikodes <thisbroisfresh@gmail.com>
2023-03-19 22:12:50 +0100

    removed unused props

--- src/components/students/EnrollExistingStudentForm.tsx
+++ src/components/students/EnrollExistingStudentForm.tsx
@@ -210,26 +210,6 @@ const EnrollExistingStudentForm = ({ handleClose, studentId }: IProps) => {
   //   }
   // };
 
-  // const props: UploadProps = {
-  //   name: "file",
-  //   multiple: true,
-  //   action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
-  //   onChange(info) {
-  //     const { status } = info.file;
-  //     if (status !== "uploading") {
-  //       console.log(info.file, info.fileList);
-  //     }
-  //     if (status === "done") {
-  //       message.success(`${info.file.name} file uploaded successfully.`);
-  //     } else if (status === "error") {
-  //       message.error(`${info.file.name} file upload failed.`);
-  //     }
-  //   },
-  //   onDrop(e) {
-  //     console.log("Dropped files", e.dataTransfer.files);
-  //   },
-  // };
-
   const normFile = (e: any) => {
     console.log("Upload event:", e);
     if (Array.isArray(e)) {
