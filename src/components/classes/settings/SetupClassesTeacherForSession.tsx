6c7fd9e5c6391516189b0d6368a82affaf10018b
kaasikodes <thisbroisfresh@gmail.com>
2023-03-29 23:08:05 +0100

    added the ability to set class teachers from session setup

--- src/components/classes/settings/SetupClassesTeacherForSession.tsx
+++ src/components/classes/settings/SetupClassesTeacherForSession.tsx
@@ -1,12 +1,15 @@
 import { Form, Button, Typography, Select, Spin } from "antd";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useAuthUser } from "react-auth-kit";
 import { IAuthDets } from "../../../appTypes/auth";
 import { GlobalContext } from "../../../contexts/GlobalContextProvider";
 import { generalValidationRules } from "../../../formValidation";
 import { IAStaffTHSClass } from "../../../helpers/courses";
 import { openNotification } from "../../../helpers/notifications";
-import { useFetchClasses } from "../../../helpersAPIHooks/classes";
+import {
+  useFetchClasses,
+  useFetchClassTeacherRecordsPerSession,
+} from "../../../helpersAPIHooks/classes";
 import { useAssignStaffToHandleSessionClasses } from "../../../helpersAPIHooks/courses";
 import { useFetchAllStaff } from "../../../helpersAPIHooks/staff";
 import { IClassEntry } from "../ClassesTable";
@@ -26,7 +29,7 @@ const SetupClassesTeacherForSession = () => {
 
   const [staffHint, setStaffHint] = useState<string | undefined>();
 
-  const { data: levelsData } = useFetchClasses({
+  const { data: levelsData, isSuccess: isLevelSuccess } = useFetchClasses({
     schoolId,
     token,
     pagination: {
@@ -97,11 +100,33 @@ const SetupClassesTeacherForSession = () => {
       });
     }
   };
+  const [form] = Form.useForm();
+  const {
+    data: courseTeacherRecords,
+    isSuccess: isCourseTeacherRecordSuccess,
+  } = useFetchClassTeacherRecordsPerSession();
+
+  useEffect(() => {
+    if (isLevelSuccess && isCourseTeacherRecordSuccess) {
+      levelsData?.data.forEach((item: IClassEntry) => {
+        const record = courseTeacherRecords.data.find(
+          (val) => val.levelId === item.id
+        );
+        form.setFieldValue(item.id, record?.staffId);
+      });
+    }
+  }, [
+    form,
+    isLevelSuccess,
+    isCourseTeacherRecordSuccess,
+    levelsData,
+    courseTeacherRecords,
+  ]);
   return (
     <div>
       {" "}
       <div className="mt-6">
-        <Form onFinish={handleFinish} requiredMark={false}>
+        <Form onFinish={handleFinish} requiredMark={false} form={form}>
           <div className="flex items-center justify-end">
             <Button type="primary" htmlType="submit" loading={isLoading}>
               Save Class Teachers
@@ -113,40 +138,42 @@ const SetupClassesTeacherForSession = () => {
             </Typography.Title>
 
             <div className="grid md:grid-cols-4 grid-cols-1 gap-4">
-              {levelsData?.data.map((item: IClassEntry) => (
-                <Form.Item
-                  name={`${item.id}`}
-                  label={item.name}
-                  labelCol={{ span: 24 }}
-                  // wrapperCol={{ span: 24 }}
-                  rules={generalValidationRules}
-                >
-                  <Select
-                    onSearch={(val) => setStaffHint(val)}
-                    showSearch
-                    value={staffHint}
-                    defaultActiveFirstOption={false}
-                    showArrow={false}
-                    filterOption={false}
-                    // onChange={handleChange}
-                    notFoundContent={null}
+              {isLevelSuccess &&
+                isCourseTeacherRecordSuccess &&
+                levelsData?.data.map((item: IClassEntry) => (
+                  <Form.Item
+                    name={`${item.id}`}
+                    label={item.name}
+                    labelCol={{ span: 24 }}
+                    // wrapperCol={{ span: 24 }}
+                    rules={generalValidationRules}
                   >
-                    {/* TO DO
+                    <Select
+                      onSearch={(val) => setStaffHint(val)}
+                      showSearch
+                      value={staffHint}
+                      defaultActiveFirstOption={false}
+                      showArrow={false}
+                      filterOption={false}
+                      // onChange={handleChange}
+                      notFoundContent={null}
+                    >
+                      {/* TO DO
             Convert all select to api searchable dropdowns */}
-                    {isStSucess ? (
-                      staffData?.data.map((item) => (
-                        <Select.Option key={item.id} value={item.id}>
-                          {item.name}({item.staffNo})
-                        </Select.Option>
-                      ))
-                    ) : (
-                      <div className="flex justify-center items-center w-full">
-                        <Spin size="small" />
-                      </div>
-                    )}
-                  </Select>
-                </Form.Item>
-              ))}
+                      {isStSucess ? (
+                        staffData?.data.map((item) => (
+                          <Select.Option key={item.id} value={item.id}>
+                            {item.name}({item.staffNo})
+                          </Select.Option>
+                        ))
+                      ) : (
+                        <div className="flex justify-center items-center w-full">
+                          <Spin size="small" />
+                        </div>
+                      )}
+                    </Select>
+                  </Form.Item>
+                ))}
             </div>
           </div>
         </Form>
