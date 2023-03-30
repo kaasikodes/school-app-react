0b7174c2ce2835c07ee417d9becc39875c2f31d8
kaasikodes <thisbroisfresh@gmail.com>
2023-03-30 23:40:59 +0100

    reactored code structure for adding staff as class session teacher

--- src/components/courses/settings/AssignStaffToCourse.tsx
+++ src/components/courses/settings/AssignStaffToCourse.tsx
@@ -1,97 +1,30 @@
-import {
-  Button,
-  Col,
-  Collapse,
-  Form,
-  message,
-  Row,
-  Select,
-  Steps,
-  Tag,
-  Typography,
-} from "antd";
-import React, { useState } from "react";
+import { Button, Collapse, message, Steps, Tag, Typography } from "antd";
+import { useState } from "react";
+import { AssignStaffAsSessionTeacher } from "./AssignStaffAsSessionTeacher";
+import SelectClassForm from "./SelectClassForm";
 
 const { Step } = Steps;
 const { Panel } = Collapse;
 
 const AssignStaffToCourse = () => {
-  const [currentClass, setCurrentClass] = useState("");
+  const [currentClass, setCurrentClass] = useState<number>();
+  const handleCurrentClass = (val: number) => {
+    setCurrentClass(val);
+  };
 
   const steps = [
     {
       title: "Select Course",
-      content: (
-        <div className="w-full flex flex-col gap-4">
-          <Typography.Title level={5}>
-            Select the class for which you would like to assign course teachers
-          </Typography.Title>
-          <div className=" flex  justify-center ">
-            <Select
-              className="w-3/4"
-              placeholder="Select a class"
-              onChange={(val) => setCurrentClass(val)}
-            >
-              <Select.Option key={2} value="Test 1">
-                Test 1
-              </Select.Option>
-              <Select.Option key={3} value="Test 3">
-                Test 3
-              </Select.Option>
-            </Select>
-          </div>
-        </div>
-      ),
+      content: <SelectClassForm handleCurrentClass={handleCurrentClass} />,
     },
     {
       title: "Assign teachers to courses",
-      content: (
-        <div className="w-full flex flex-col gap-4">
-          {/* "list of courses taught in the class selected, then a multiple select to allow teachers to be assigned then when teachers >= 1, a select populated from teachers will know allow for selection of primary teacher" */}
-          <Typography.Title level={5}>
-            Assign teachers to the courses taught in {currentClass}
-          </Typography.Title>
-          <Form labelCol={{ span: 4 }}>
-            {["Physics", "Chemistry", "Geography"].map((item) => (
-              <Row gutter={4}>
-                <Col span={4}>
-                  <span>{item}</span>
-                </Col>
-                <Col span={20}>
-                  <div className="flex gap-2">
-                    <Form.Item className="flex-1">
-                      <Select placeholder="Select teacher(s)" mode="tags">
-                        <Select.Option key={2} value="Test 1">
-                          James Dauda
-                        </Select.Option>
-                        <Select.Option key={3} value="Test 3">
-                          Matt Walsh
-                        </Select.Option>
-                      </Select>
-                    </Form.Item>
-                    <Form.Item className="flex-1">
-                      <Select placeholder="Select primary teacher">
-                        <Select.Option key={2} value="Test 1">
-                          James Dauda
-                        </Select.Option>
-                        <Select.Option key={3} value="Test 3">
-                          Matt Walsh
-                        </Select.Option>
-                      </Select>
-                    </Form.Item>
-                  </div>
-                </Col>
-              </Row>
-            ))}
-          </Form>
-        </div>
-      ),
+      content: <AssignStaffAsSessionTeacher />,
     },
     {
       title: "Confirm Selection",
       content: (
         <div>
-          {/* "The Class selcted will be shown, alongside the courses as an accordian with each accod showing teachers , and primary teacher" */}
           <Typography.Title level={5}>{currentClass} Courses</Typography.Title>
           <div className="flex flex-col gap-4">
             <Collapse accordion>
