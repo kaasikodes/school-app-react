427656b32ccddbf17300b135996b350af36116f5
kaasikodes <thisbroisfresh@gmail.com>
2023-04-03 10:59:35 +0100

    invite ui components

--- src/components/invitations/InvitationTable.tsx
+++ src/components/invitations/InvitationTable.tsx
@@ -1,18 +1,48 @@
 import { Table } from "antd";
-import React from "react";
+import { ColumnsType, TablePaginationConfig, TableProps } from "antd/lib/table";
 
-export const InvitationTable = () => {
+import React from "react";
+import { TInvite } from "../../helpersAPIHooks/invites/useFetchInvitations";
+import moment from "moment";
+
+export const InvitationTable: React.FC<{
+  data?: TInvite[];
+  loading: boolean;
+  pagination?: TablePaginationConfig;
+  onChange?: TableProps<TInvite>["onChange"];
+}> = ({ data, onChange, pagination, loading }) => {
+  const columns: ColumnsType<TInvite> = [
+    { title: "Email", dataIndex: "email" },
+    { title: "Invitation Code", dataIndex: "code" },
+    {
+      title: "Sent",
+      dataIndex: "updatedAt",
+      render: (val) => moment(val).fromNow(),
+    },
+    {
+      title: "Accepted",
+      dataIndex: "accepted",
+      render: (val) => (
+        <span className="capitalize font-semibold">
+          {val === 0 ? "No" : "Yes"}{" "}
+        </span>
+      ),
+    },
+
+    {
+      title: "Accepted At",
+      dataIndex: "acceptedAt",
+      render: (val) => (val ? moment(val).fromNow() : ""),
+    },
+  ];
   return (
     <div>
       <Table
-        columns={[
-          { title: "Email" },
-          { title: "Invitation Code" },
-          { title: "Sent On" },
-          { title: "Accepted" },
-
-          { title: "Accepted On" },
-        ]}
+        columns={columns}
+        dataSource={data}
+        onChange={onChange}
+        pagination={pagination}
+        loading={loading}
       />
     </div>
   );
