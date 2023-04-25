dc6b580b4bcc44878e68160995cde156010733ce
kaasikodes <thisbroisfresh@gmail.com>
2023-04-25 23:25:40 +0100

    added padding to comp loader

--- src/components/loaders/ComponentLoader.tsx
+++ src/components/loaders/ComponentLoader.tsx
@@ -2,7 +2,7 @@ import BarLoader from "react-spinners/BarLoader";
 
 const ComponentLoader = ({ height = "h-72" }: { height?: string }) => {
   return (
-    <div className={`${height}  w-full flex justify-center items-center`}>
+    <div className={`${height} pt-4  w-full flex justify-center items-center`}>
       <BarLoader color="#0080FE" />
     </div>
   );
